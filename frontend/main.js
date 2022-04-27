35745a3a7ce2e3ec37fafe672897492df417f811
unknown <dongmokevin3@gmail.com>
2022-04-27 12:52:04 +0200

    updates to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+getVisitCount();
+})
+
+const funtionApi = '';
+
+const getVisitCount = () => {
+    let getVisitCount=30; 
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("website called function API");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(erro){
+        console.log(error);
+    });
+    return count;
+}
